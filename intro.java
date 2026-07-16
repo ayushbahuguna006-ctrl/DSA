86665a399a3e2ae5789a6eda93b0cd9fa92c14e4
Ayush <ayushbahuguna006@gmail.com>
2026-07-16 11:28:48 +0530

    notes updated

---
+++ intro.java
@@ -0,0 +1,17 @@
+types of programming languages:
+
+procedural~series of well structured program to complete task
+functional~writing program in pure functions,first class functions
+object oriented~!revolves around objects,code+data=object
+
+Static vs Dynamic languages:
+Static:
+~perform type checking at run time
+~errors will show at runtime
+~declare datatype before you use it
+
+Dynamic:
+~perform type checking at compile time
+~errors will show at on program execution
+~no need of declaration of datatypes
+
