68c08ff667cfd61fb7b71606aeae95d61cde57bb
Ayush <ayushbahuguna006@gmail.com>
2026-08-01 18:13:26 +0530

    notes updation

--- 5conditionloop.java
+++
@@ -1,5 +0,0 @@
-public class conditionloop {
-    public static void main(String[] args) {
-        
-    }
-}
---
+++ conditionloop.java
@@ -0,0 +1,31 @@
+
+//if-else statements
+
+// public class conditionloop {
+//     public static void main(String[] args) {
+//         int salary=40000;
+//         if(salary>10000){
+//             salary+=10000;
+//             System.out.println(salary);
+//         }
+//         else if(salary>26000){
+//             salary+=5000;
+//             System.out.println(salary);
+//         }
+//         else{
+//             System.out.println(salary);
+//         }
+//     }
+// }
+
+
+
+//loops~for loop,while loop.do-while loop
+
+public class conditionloop {
+    public static void main(String[] args) {
+        for(int i=0;i!=6;i++){
+            System.out.println(i);
+        }  
+}
+}
