6c4b64f87c38bf239bee2305b20b5b9a3ac974da
Ayush <ayushbahuguna006@gmail.com>
2026-08-07 19:57:15 +0530

    notes updation

--- 8astring.java
+++ 8astring.java
@@ -1,9 +1,10 @@
 //Strings are immutable
+//String is a class in java
 //In java,outputs are always given in form of string whatever you int a=56 or int[] arr=new int{1,3,4,4} everything output is string as tostring method is used when giving the output.Also in case of arrays output is given in form of string in formatted text so we generally discard the tostring method and use the array.toString() method as it gives a much more readable format although we can make our own method later on
 // String name="Ayush";   //here string is the datatypes name is the reference variable and ayush is the object in the string pool in the heap memory.
 //we can't change string objects if once made due to security reasons as if one object changed by user and multiple ref variable are pointing to the same object it would cause passwords to change for each ref variable which are the user say .
 //Second thing we know if two ref variable point to the same object and we try to change the value of one of the ref variable it causes value to be changed for other ref variable but for strings it doesn't work this way as in java strings are immutable. 
-
+//if  string you write a method on a string you need to store it in a variable cuz it won't change the original it creates a new string while stringbuilder works on same string it we don't need to store it in a variable it changes the original string
 
 // public class astring{
 //     public static void main(String[] args) {
@@ -24,7 +25,7 @@
 // }
 
 // In the above example , we should know that string objects are immutable i.e. ayush here so when we are assigning new object  to a new variable a , it creates a new object so a ref variable points to this new object and garbage collection hits and Ayush object is removed
-//a==b,gives true for if the ref variale both a and b are poiting to the same object else it gives false if the ref variable are pointing uniquely even if the object is the same.
+//a==b,gives true for if the ref variale both a and b are poiting to the same object else it gives false if the ref variable are pointing uniquely even if the object is the same.so equal equal checks memory addresses i.e. references and eqauls to explicitly checks values only.
 //if you use the new method for creating a string it will store the object in the heap memory outside of the string pool and thus even if two objects are same ref variable will be pointing to the object uniquely for both say a for object 1 and b for object 2.
 
 // public class astring{
@@ -151,9 +152,9 @@
 // deleteCharAt(int index)     //deletes one character
 // reverse()    //reverse the string
 // charAt(int index)   //returns a char at the specified index
-// length()    //return length of string
+// length()    //say name.length() //parnthesis should be there in case of string methods   //return length of string
 // substring(int start,int end)    //returns substring from start to end-1
-// toString()     //converts it into string
+// toString()     //converts it into a new different string from stringbuilder
 
 
 //String methods ~
@@ -175,3 +176,29 @@
 
 
 // Ques.Palindrome program
+
+// public class astring{
+//     public static void main(String[] args) {
+//         String str="madam";
+//         Boolean ans=isPlaindrome(str);
+//         System.out.println(ans);
+//     }
+//     static boolean isPlaindrome(String str){
+//         StringBuilder st=new StringBuilder(str);
+//         st.reverse();
+
+//         //incorrect
+
+//         // if(str==st){
+//         //     return true;      
+//         // }                        //You can't use == for string and stringbuilder as both are different types
+//         // else{
+//         //     return false;
+//         // }
+
+//         //corrected
+
+//         return str.equals(st.toString());
+     
+//     }
+// }
